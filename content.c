fc02d819f717085abb8bfdf58faef749639d2b85
Begatim <begatim@begatimi_95@outlook.com>
2022-02-25 14:45:24 +0000

    add code in files types: .c,.h,.cpp,.ml, remove code in files types: .c,.h,.cpp,.ml,

--- content.c
+++ content.c
@@ -1,6 +1,6 @@
 # include <stdio.h>
 
 int main() {
-        printf("csymg");
+        printf("oqdct");
 return 0;
 }
--- content.cpp
+++ content.cpp
@@ -183,6 +183,6 @@ int main() {
         cout << "nshey";
         cout << "pygxm";
         cout << "rwdej";
-        cout << "hqiap";
+        cout << "wpikm";
 return 0;
 }
